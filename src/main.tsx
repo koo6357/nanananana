c3987f10dc1498f7a9fa69c2d5a97f5ad0cecc34
koo6357 <koo6357@naver.com>
2024-05-26 15:09:58 +0900

    fix: 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client' // react 로 개발된 JSX 를 실제 dom 으로 컴파일해서 렌더
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <div>Hello!!!</div>
+    <div>Hello!!!
+      <p>수정합니다!</p>
+    </div>
 )
 
